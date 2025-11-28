d91a1a079567cc9470117abade6a98e96841cdd8
iandiv <dvineian1239@gmail.com>
2025-11-28 13:17:38 +0800

    refactor: update sidebar border and clean up CSS comments

--- src/App.tsx
+++ src/App.tsx
@@ -68,7 +68,7 @@ const Sidebar: React.FC<SidebarProps> = ({
       </button>
       {/* Sidebar */}
       <div
-        className={`fixed left-0 top-0 h-full w-80 bg-background/95 backdrop-blur-sm border-r flex flex-col overflow-y-auto transition-transform duration-300 ease-in-out z-40 ${
+        className={`fixed left-0 top-0 h-full w-80 bg-background/95 backdrop-blur-sm border-r border-[#e5e7eb] flex flex-col overflow-y-auto transition-transform duration-300 ease-in-out z-40 ${
           isOpen ? 'translate-x-0' : '-translate-x-full'
         }`}
       >
